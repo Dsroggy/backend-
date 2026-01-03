a1869ec676bfb8b29fe9aab58189444b87dd120f
Dsr Oggy <109042065+Dsroggy@users.noreply.github.com>
2026-01-03 22:24:18 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
-export default function handler(req, res) {
-  res.status(200).send("DSR API WORKING");
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    return res.status(200).send("DSR API WORKING");
+  }
+
+  if (req.method === "POST") {
+    return res.status(200).json({ ok: true });
+  }
+
+  return res.status(200).send("OK");
 }
